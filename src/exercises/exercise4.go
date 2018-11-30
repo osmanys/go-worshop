c8a3e4446161f2d2ef821bb12c61b797de9d0487
osmanys <osmanys@gmail.com>
2018-11-30 01:24:49 -0300

    add .gitignore

--- src/exercises/exercise4.go
+++ src/exercises/exercise4.go
@@ -1,29 +1,36 @@
 package exercises
 
+// Person interface
 type Person interface {
 	Pay(prize float64) float64
 }
 
+// Jubilado person struct
 type Jubilado struct {
 	name string
 }
 
+// Normal person struct
 type Normal struct {
 	Jubilado
 }
 
+// Invitado person struct
 type Invitado struct {
 	Jubilado
 }
 
+// Pay Jubilado func
 func (p Jubilado) Pay(prize float64) float64 {
 	return 0.75 * prize
 }
 
+// Pay Normal func
 func (p Normal) Pay(prize float64) float64 {
 	return prize
 }
 
+// Pay Invitado func
 func (p Invitado) Pay(prize float64) float64 {
 	return 0
 }
